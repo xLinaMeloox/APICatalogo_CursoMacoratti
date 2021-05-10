299bda73cbf2b5f280163d4fb7715f5014d6b479
Lina Melo <lina@w12.com.br>
2021-05-10 09:21:54 -0300

    Criando Models e Controller

--- APICatalogo_CursoMacoratti/Models/Produto.cs
+++ APICatalogo_CursoMacoratti/Models/Produto.cs
@@ -7,5 +7,12 @@ namespace APICatalogo_CursoMacoratti.Models
 {
     public class Produto
     {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }    
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
     }
 }
